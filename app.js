fd3485c9e47b5f1aa30ba438befd80056d54d181
VINICIUS JULIO DE OLIVEIRA <25132898@CFP127ALUNO.LOCAL>
2025-10-20 14:05:15 -0300

    Botão ?

--- app.js
+++ app.js
@@ -5,20 +5,58 @@
 async function pesquisarPokemon(pokemon) {
     const responseApi = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
     const dados = await responseApi.json()
-    console.log(dados)
     return dados
-    
+
 }
 
 
-
+const container = document.getElementById('container')
 
 
 
 const botaoListar = document.getElementById('btnLista')
-if(botaoListar){  
+if (botaoListar) {
     botaoListar.addEventListener('click', () => {
-    window.location.href = './page2/page2.html'
-})
+        window.location.href = './page2/page2.html'
+    })
 }
+const botaoInterrogacao = document.getElementById('interrogacao')
+if (botaoInterrogacao) {
+    botaoInterrogacao.addEventListener('click', async () => {
+        await popUp()
+
+    })
+}
+document.addEventListener('click', () => {
+    container.replaceChild()
+})
+
+
+
+async function popUp() {
+    let poke = await pesquisarPokemon('pikachu')
+
+    container.replaceChildren()
+
+
+    const fundoBranco = document.createElement('div')
+    fundoBranco.className = 'fundoBranco'
+
+    let img = document.createElement('img')
+    img.src = poke.sprites.other['showdown'].front_default
+
+    fundoBranco.appendChild(img)
+    container.appendChild(fundoBranco)
+
+    document.addEventListener('click', function fecharPopup(e) {
+        
+        if (!fundoBranco.contains(e.target) && e.target !== botaoInterrogacao) {
+            fundoBranco.remove()               
+            document.removeEventListener('click', fecharPopup) 
+        }
+    })
+
+
+}
+
 
